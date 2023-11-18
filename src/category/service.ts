4636a4952d00224a690f66fa6d7795de0018bfd7
nacir1i <taoufiqnaciri234@gmail.com>
2023-11-18 01:03:29 +0100

    create categories services

---
+++ src/category/service.ts
@@ -0,0 +1,30 @@
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import categories from "../db/schema/category";
+import { CreateCategory, UpdateCategory } from "./schemas";
+import { isArrayEmpty } from "../utils/utils";
+
+export async function createOne(category: CreateCategory) {
+  return db?.insert(categories).values(category);
+}
+
+export async function findOne(id: number) {
+  const category = await db
+    ?.select()
+    .from(categories)
+    .where(eq(categories.id, id));
+
+  if (isArrayEmpty(category)) return null;
+  return await db?.select().from(categories).where(eq(categories.id, id));
+}
+
+export async function findAll() {
+  return await db?.select().from(categories);
+}
+
+export async function updateOne(category: UpdateCategory, id: number) {
+  return await db
+    ?.update(categories)
+    .set(category)
+    .where(eq(categories.id, id));
+}
