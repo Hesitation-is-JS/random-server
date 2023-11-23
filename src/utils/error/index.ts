7228803139f11e4cc1da16bd84703bf59e135eb3
nacir1i <taoufiqnaciri234@gmail.com>
2023-11-23 21:10:18 +0100

    update error handling

---
+++ src/utils/error/index.ts
@@ -0,0 +1,45 @@
+import { FastifyError, FastifyRequest, FastifyReply } from "fastify";
+import DrizzleErrorHandler, { DrizzleError, isDrizzleError } from "./drizzle";
+import HttpError from "./http";
+
+export default function globalErrorHandler(
+  this: any,
+  error: FastifyError,
+  _req: FastifyRequest,
+  rep: FastifyReply
+) {
+  let status = 500;
+  let message = "Internal server  error";
+
+  switch (true) {
+    case error instanceof HttpError:
+      status = error.status;
+      message = error.message;
+      break;
+
+    case isDrizzleError(error):
+      const drizzleErrorResponse = new DrizzleErrorHandler(
+        error as unknown as DrizzleError
+      ).getResponse();
+
+      status = drizzleErrorResponse.status;
+      message = drizzleErrorResponse.message;
+      break;
+
+    case error instanceof Error:
+      status = error.statusCode ?? status;
+      message = error.message;
+      break;
+
+    default:
+      this.log.error(error);
+      break;
+  }
+
+  rep.code(status).send({ message: message, success: false });
+}
+
+export interface ErrorResponse {
+  message: string;
+  status: number;
+}
