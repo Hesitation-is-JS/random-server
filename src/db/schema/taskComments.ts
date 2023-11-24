4c551cff6927c714fdbdcf092223955aa9d40c53
nacir1i <taoufiqnaciri234@gmail.com>
2023-11-24 19:03:58 +0100

    add autoicrement to task comment db schema

--- src/db/schema/taskComments.ts
+++ src/db/schema/taskComments.ts
@@ -3,11 +3,11 @@ import { mysqlTable, varchar, timestamp, int } from "drizzle-orm/mysql-core";
 import users from "./user";
 import tasks from "./task";
 
-const tasksComments = mysqlTable("tasks-comments", {
-  id: int("id").primaryKey(),
+const tasksComments = mysqlTable("tasks_comments", {
+  id: int("id").primaryKey().autoincrement(),
   userId: varchar("user_id", { length: 256 }).references(() => users.userId),
-  taskId: int("tas_id").references(() => tasks.id),
-  content: varchar("user_id", { length: 256 }).notNull(),
+  taskId: int("task_id").references(() => tasks.id),
+  content: varchar("content", { length: 256 }).notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
 });
