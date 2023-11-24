e81ad602bd398cb71ab9c84b06f82e9a2f4cf75c
nacir1i <taoufiqnaciri234@gmail.com>
2023-11-24 18:05:30 +0100

    create task commets db schema

---
+++ src/db/schema/taskComments.ts
@@ -0,0 +1,15 @@
+import { sql } from "drizzle-orm";
+import { mysqlTable, varchar, timestamp, int } from "drizzle-orm/mysql-core";
+import users from "./user";
+import tasks from "./task";
+
+const tasksComments = mysqlTable("tasksComments", {
+  id: int("id").primaryKey(),
+  userId: varchar("user_id", { length: 256 }).references(() => users.userId),
+  taskId: int("tas_id").references(() => tasks.id),
+  content: varchar("user_id", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
+});
+
+export default tasksComments;
