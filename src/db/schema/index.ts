f15a2991b60653d663a2be62212dd5eea3e636b4
nacir1i <taoufiqnaciri234@gmail.com>
2023-11-28 07:54:58 +0100

    update task to task relation

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -6,7 +6,8 @@ import {
   timestamp,
   datetime,
   mysqlEnum,
-  AnyMySqlColumn,
+  index,
+  foreignKey,
 } from "drizzle-orm/mysql-core";
 
 export const categories = mysqlTable("categories", {
@@ -32,20 +33,38 @@ export const statesRelations = relations(states, ({ many }) => ({
   tasks: many(tasks),
 }));
 
-export const tasks = mysqlTable("tasks", {
-  id: int("id").primaryKey().autoincrement(),
-  parentId: int("parent_id").references((): AnyMySqlColumn => tasks.id),
-  title: varchar("title", { length: 256 }),
-  description: varchar("description", { length: 2000 }),
-  dueDate: datetime("due_date"),
-  stateId: int("state_id").references(() => states.id),
-  categoryId: int("category_id").references(() => categories.id),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
-});
+export const tasks = mysqlTable(
+  "tasks",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    parentId: int("parent_id"),
+    title: varchar("title", { length: 256 }),
+    description: varchar("description", { length: 2000 }),
+    dueDate: datetime("due_date"),
+    stateId: int("state_id").references(() => states.id),
+    categoryId: int("category_id").references(() => categories.id),
+    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => ({
+    parentReference: foreignKey({
+      columns: [table.parentId],
+      foreignColumns: [table.id],
+    }),
+    parentIdx: index("parent_idx").on(table.parentId),
+    stateIdx: index("state_idx").on(table.stateId),
+    categoryIdx: index("category_idx").on(table.categoryId),
+  })
+);
 
 export const tasksRelations = relations(tasks, ({ many, one }) => ({
   users: many(users),
+  tasks: many(tasks, { relationName: "subtaskToTask" }),
+  parent: one(tasks, {
+    fields: [tasks.parentId],
+    references: [tasks.id],
+    relationName: "taskToSubtask",
+  }),
   category: one(categories),
   state: one(states),
   comments: many(tasksComments),
