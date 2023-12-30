3f4265fa989434849ccae4a46aa47a4df4dccfc0
nacir1i <taoufiqnaciri234@gmail.com>
2023-12-30 15:13:10 +0100

    delete create user schema

--- src/user/schemas.ts
+++ src/user/schemas.ts
@@ -1,25 +1,9 @@
 import { users } from "../db/schema";
 import { ValidationSchema } from "../utils/interfaces";
-import { defaultSuccessSchema, findOneResponseSchema } from "../utils/schemas";
 
 export type User = typeof users.$inferSelect;
 export type CreateUser = typeof users.$inferInsert;
 export type UpdateUser = { theme?: "DARK" | "LIGHT" | null | undefined };
-
-export const createUserSchema: ValidationSchema = {
-  description: "This endpoint handles the creation of user records",
-  tags: ["user"],
-  summary: "Create user record",
-  body: {
-    type: "object",
-    properties: {
-      userId: { type: "string" },
-      theme: { type: "string" },
-    },
-    required: ["userId"],
-  },
-  response: defaultSuccessSchema,
-};
 
 export const finOneUserSchema: ValidationSchema = {
   description: "This endpoint return user data by their corresponding id",
